b17ee57f2f092ee30bf324ae0159e8dbf263957f
Yudy <cn.t0604@gmail.com>
2024-02-06 08:51:04 -0500

    Ejercicio_Ciclo_While

--- bucles.py
+++ bucles.py
@@ -1,1 +1,17 @@
-print("Hola")
+import random
+
+Secreto = random.randint(1,10)
+Numero = int(input("Adivine un numero del 1 al 10"))
+
+while Numero != Secreto:
+
+    if Numero < Secreto:
+        print("El numero es mayor")
+
+    else:
+        print("El numero es menor")
+    
+    Numero = int(input("Adivinar el numero"))
+
+
+print ("Has adivinado el numero")
