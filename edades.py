982cdeab54abf1d5b56aacce42aada0ca74422d0
Yudy <cn.t0604@gmail.com>
2024-02-07 09:08:35 -0500

    edades2

--- edades.py
+++ edades.py
@@ -1,24 +1,21 @@
 sumam = 0
 sumat = 0
 suman = 0
-
+ 
 print("TURNO MAÑANA")
 for i in range (1,7):
-    print("Estudiante" , i)
-    mañana = int(input("Ingrese la edad: "))
+    mañana = int(input(f"Ingrese la edad del estudiante {i}"))
     sumam = sumam + mañana
     promm = sumam / 6
 print("TURNO TARDE")
-for i1 in range (1,8):
-    print("Estudiante" , i1)
-    tarde = int(input("Ingrese la edad: "))
+for i in range (1,8):
+    tarde = int(input(f"Ingrese la edad del estudiante {i}"))
     sumat = sumat + tarde
     promt = sumat / 7
 
 print("TURNO NOCHE")
-for i2 in range (1,13):
-    print("Estudiante" , i2)
-    noche = int(input("Ingrese la edad: "))
+for i in range (1,13):
+    noche = int(input(f"Ingrese la edad del estudiante {i}"))
     suman = suman + noche
     promn = suman / 12
 
