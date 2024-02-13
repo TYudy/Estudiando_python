bc9590e97c8bc3202e4278ebc7d0c2e5849da16f
Vanne <cn.t0604@gmail.com>
2024-02-12 21:49:33 -0500

    diccionario

---
+++ diccionario.py
@@ -0,0 +1,16 @@
+'''diccionario = {"azul":"blue", "rojo":"red", "verde":"green"}
+diccionario["amarillo"]="yellow"
+del (diccionario["verde"])
+print(diccionario["azul"])
+print(diccionario)
+
+diccionario2 = {"julio":[23,1.80],"Pablo":[25,1.90], "Raquel":{"edad":22,"estatura":1.60}}'''
+
+diccionario3 = {}
+
+for i in range (1,5):
+    id = int(input("Por favor ingrese su id"))
+    señor=input("POr favor ingrese su nombre")
+    diccionario3 [id] = señor
+
+print(diccionario3)
